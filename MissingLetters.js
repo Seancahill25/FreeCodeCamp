80246969be13d34bceaeb286d287d71e3d538541
Sean Cahill <seancahill25@gmail.com>
2017-12-12 17:27:30 +0000

    solved missing letters problem

---
+++ MissingLetters.js
@@ -0,0 +1,10 @@
+function fearNotLetter(str) {
+  for (var i = 0; i < str.length - 1; i++)
+    if(str.charCodeAt(i + 1) == str.charCodeAt(i) + 1) {
+    } else {
+      return String.fromCharCode(str.charCodeAt(i) + 1);
+    }
+}
+
+
+console.log(fearNotLetter("ac"));
