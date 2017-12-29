1078535f6f2faf6088a255ad5270eeefdbbab1e6
Sean Cahill <seancahill25@gmail.com>
2017-12-29 23:45:32 +0000

    cleaned up code

--- RomanNumeralConverter.js
+++ RomanNumeralConverter.js
@@ -1,18 +1,18 @@
 var convertToRoman = function(num) {
 
-  var decimalValue = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ];
+  var numbers = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ];
   var romanNumeral = [ 'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I' ];
 
-  var romanized = '';
+  var converted = '';
 
-  for (var index = 0; index < decimalValue.length; index++) {
-    while (decimalValue[index] <= num) {
-      romanized += romanNumeral[index];
-      num -= decimalValue[index];
+  for (var i = 0; i < numbers.length; i++) {
+    while (numbers[i] <= num) {
+      converted += romanNumeral[i];
+      num -= numbers[i];
     }
   }
 
-  return romanized;
+  return converted;
 };
 
 convertToRoman(36);
